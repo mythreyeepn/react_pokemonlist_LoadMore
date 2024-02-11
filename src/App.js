2b296946132edc6e6e5b4ea4d618934bce39ca5c
Mythreyee Pingala <mythreyeepn@gmail.com>
2024-02-10 21:00:41 -0600

    logic added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [displayList, setDisplayList] = useState([]);
+  const [totalResults, setTotalResults] = useState([]);
+  const [num, setNum] = useState(6);
+
+  useEffect(() => {
+    const results = fetch("https://pokeapi.co/api/v2/pokemon/")
+      .then((response) => response.json())
+      .then((data) => {
+        setTotalResults(data.results);
+        let newResults = [...data.results];
+        setDisplayList(newResults.splice(0, num));
+      });
+  }, [num]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <p>Pokemon List</p>
       </header>
+      <div className="container">
+        {displayList &&
+          displayList.length > 0 &&
+          displayList.map((item, index) => {
+            return (
+              <div className="item" key={index}>
+                {item.name}
+              </div>
+            );
+          })}
+      </div>
+      <button
+        disabled={displayList.length === totalResults.length}
+        onClick={() => setNum(num + 6)}
+      >
+        {" "}
+        Load More
+      </button>
     </div>
   );
 }
